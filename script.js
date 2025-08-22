0779ee204acdf503bea6d189e17d7069b8719dcd
rwonderwhiz111-maker <rwonderwhiz111@gmail.com>
2025-08-22 12:29:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const memes = {
+    happy: [
+        "https://i.imgflip.com/30b1gx.jpg",
+        "https://i.imgflip.com/1bij.jpg",
+        "https://i.imgflip.com/26am.jpg",
+        "https://i.imgur.com/9bknx7F.jpg" // <--- Added new meme here!
+    ],
+    sad: [
+        "https://i.imgflip.com/2fm6x.jpg",
+        "https://i.imgflip.com/1ur9b0.jpg",
+        "https://i.imgflip.com/3vzej.jpg"
+    ],
+    // ...bored and excited
+};
