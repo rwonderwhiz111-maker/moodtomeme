99be228829f76e6336932594c458b3bac531de2e
rwonderwhiz111-maker <rwonderwhiz111@gmail.com>
2025-08-22 12:37:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const memes = {
     happy: [
-        "https://i.imgflip.com/30b1gx.jpg",
-        "https://i.imgflip.com/1bij.jpg",
-        "https://i.imgflip.com/26am.jpg",
-        "https://i.imgur.com/9bknx7F.jpg" // <--- Added new meme here!
+       "https://imgur.com/gallery/evil-larry-cat-meme-9uI5noU#J59Afar",
+        "https://imgur.com/gallery/jet2-holiday-38lY30Z#vtPdJnQ" // <--- Added new meme here!
     ],
     sad: [
-        "https://i.imgflip.com/2fm6x.jpg",
-        "https://i.imgflip.com/1ur9b0.jpg",
-        "https://i.imgflip.com/3vzej.jpg"
+        "https://tenor.com/poSAbiSDcvB.gif",
+    
     ],
     // ...bored and excited
 };
